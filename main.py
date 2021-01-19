31a8338faed649a652aca1000758afae2f56e46e
drivenbydesire1110 <potdukhesandeep7gmail.com>
2021-01-19 17:04:40 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import sys
-import img2pdf
-import os
 
-filepath = sys.argv[1]
-if os.path.isdir(filepath):
-    with open("output.pdf", "wb") as f:
-        imgs = []
-        for fname in os.listdir(filepath):
-            if not fname.endswith(".jpg"):
-                continue
-            path = os.path.join(filepath, fname)
-            if os.path.isdir(path):
-                continue
-            imgs.append(path)
-        f.write(img2pdf.convert(imgs))
-elif os.path.isfile(filepath):
-    if filepath.endswith(".jpg"):
-        with open("output.pdf", "wb") as f:
-            f.write(img2pdf.convert(filepath))
-else:
-    print("please input file or dir")
+
+# importing necessary libraries 
+import img2pdf 
+from PIL import Image 
+import os 
+
+# storing image path 
+img_path = "C:\Users\Sandeep\Desktop\mooc report\dead.jpg"
+
+# storing pdf path 
+pdf_path = "C:\Users\Sandeep\Desktop\mooc report\dead.pdf"
+
+# opening image 
+image = Image.open(img_path) 
+
+# converting into chunks using img2pdf 
+pdf_bytes = img2pdf.convert(image.filename) 
+
+# opening or creating pdf file 
+file = open(pdf_path, "wb") 
+
+# writing pdf files with chunks 
+file.write(pdf_bytes) 
+
+# closing image file 
+image.close() 
+
+# closing pdf file 
+file.close() 
+
+# output 
+print("Successfully made pdf file") 
